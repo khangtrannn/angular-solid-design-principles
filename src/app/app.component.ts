7b34aa53ba5289e313c7ba0c315c3c5fde03ae81
Khang Tran <khangtrann8198@gmail.com>
2023-07-12 10:52:49 +0000

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,40 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    
+  `,
+  styles: [
+    `
+      .widget-icon {
+        font-size: 64px;
+        width: 64px;
+        height: 64px;
+        color: orange;
+      }
+      .value {
+        font-size: 24px;
+        opacity: 0.7;
+      }
+      .widget-content {
+        display: block;
+        text-align: center;
+        position: relative;
+        min-width: 190px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'angular-solid-design-principles';
+
+  onExportJson(): void {
+    let data = JSON.stringify({ weather: { is_sunny: true, temp: '+25' } });
+    let dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(data);
+    let exportFileName = 'weather.json';
+    let linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', exportFileName);
+    linkElement.click();
+  }
 }
